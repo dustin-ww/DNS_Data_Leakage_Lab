8c53598db66a3f12875dde9c32a27269fe7a48da
dustin-ww <wickertdustin@gmail.com>
2025-08-20 11:07:01 +0200

    feat: optimize dirs on target

--- infrastructure/vagrant/create_inventory.sh
+++ infrastructure/vagrant/create_inventory.sh
@@ -7,13 +7,8 @@ vagrant ssh-config > vagrant-ssh-config
 # Erstelle Inventory mit Hostnamen
 cat > ./hosts_dynamic.ini << EOF
 [client]
-client ansible_user=vagrant ansible_python_interpreter=/usr/bin/python3
+client ansible_user=vagrant ansible_python_interpreter=/usr/bin/python3 data_source_dir=/home/dustin/Dev/DNS_Data_Leakage_Lab/data python_source_dir=/home/dustin/Dev/DNS_Data_Leakage_Lab
 
-[resolver]
-resolver ansible_user=vagrant ansible_python_interpreter=/usr/bin/python3
-
-[sniffer]
-sniffer ansible_user=vagrant ansible_python_interpreter=/usr/bin/python3
 
 [all:vars]
 ansible_ssh_common_args=-F ./vagrant/vagrant-ssh-config -o StrictHostKeyChecking=no
